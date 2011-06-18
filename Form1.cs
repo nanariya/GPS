840a1ec1470570ce266480e5e2623cc9ec4373a0
Nanariya <nanariya@280w.info>
2011-06-19 02:13:02 +0900

    NMEA GPSを簡単に使うためのC#ライブラリのつもり

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace test2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Nanariya.Gps gps = new Nanariya.Gps();
+            gps.GpsDataReceive += new Nanariya.Gps.GpsEventHandler(gps_GpsDataReceive);
+
+            gps.Start();
+            
+        }
+
+        delegate void GpsReadDelegate(Nanariya.GpsReadEventArgs data);
+        void gps_GpsDataReceive(object sender, Nanariya.GpsReadEventArgs e)
+        {
+            try
+            {
+                this.Invoke(new GpsReadDelegate(GpsRead), new object[] { e });
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void GpsRead(Nanariya.GpsReadEventArgs data)
+        {
+            richTextBox1.AppendText("N " + data.Latitude + ", E " + data.Longitude + "\r\n");
+        }
+
+    }
+}
---
+++ Gps.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO.Ports;
+
+namespace Nanariya
+{
+    class Gps
+    {
+        #region イベントハンドラの定義
+        //イベントハンドラの定義
+        public delegate void GpsEventHandler(object sender, GpsReadEventArgs e);
+        //イベントハンドラの名前
+        public event GpsEventHandler GpsDataReceive;
+        /// <summary>
+        /// イベントハンドラを発生させる為に呼ぶメソッド
+        /// </summary>
+        /// <param name="e"></param>
+        protected virtual void OnGpsDataReceive(GpsReadEventArgs e)
+        {
+            GpsDataReceive(this, e);
+        }
+        #endregion
+
+        SerialPort _serialPort = null;
+
+        public String Port { get; set; }
+        public int BaudRate { get; set; }
+        public int DataBits { get; set; }
+        public Parity Parity { get; set; }
+        public StopBits StopBits { get; set; }
+        public Handshake Handshake { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Gps()
+        {
+            //デフォルト
+            this.Port = "COM4";
+            this.BaudRate = 4800;   //PGM-111はボーレート4800がデフォ
+            this.DataBits = 8;
+            this.Parity = Parity.None;
+            this.StopBits = StopBits.One;
+            this.Handshake = Handshake.None;
+        }
+
+        public void Start()
+        {
+            _serialPort = new SerialPort(this.Port, this.BaudRate, this.Parity, this.DataBits, this.StopBits);
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(_serialPort_DataReceived);
+            //ハンドシェイクだけ後から設定
+            _serialPort.Handshake = this.Handshake;
+
+            _serialPort.Open();
+        }
+
+        /// <summary>
+        /// SerialPortのイベント受け取り
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Dictionary<String, String> eventData = new Dictionary<string, string>();
+
+            //生データを加工
+            eventData = ParseGpsPos(_serialPort.ReadLine());
+
+            //中身あるときだけイベント発生させる（仮
+            if (eventData.Count > 0)
+            {
+                GpsReadEventArgs gpsEvent = new GpsReadEventArgs(eventData);
+                OnGpsDataReceive(gpsEvent);
+            }
+        }
+
+        /// <summary>
+        /// GPGGAを抜き出してフォーマット調整
+        /// </summary>
+        /// <param name="rawData"></param>
+        /// <returns></returns>
+        private Dictionary<String, String> ParseGpsPos(String rawData)
+        {
+            Dictionary<String, String> temp = new Dictionary<string, string>();
+
+            string[] rawLines = rawData.Split('\n');
+            foreach (string line in rawLines)
+            {
+                Regex regex = new Regex("^\\$GPGGA,\\d+,\\d+.\\d+,N,\\d+.\\d+,E,1");
+                if (regex.IsMatch(line))
+                {
+                    //0:$GPGGA
+                    //1:UTC
+                    //2:Latitude
+                    //3:N or S
+                    //4:Longitude
+                    //5:E or W
+                    //6:Dis Enable
+                    //7:キャッチしてる衛星の数
+                    //8:?
+                    //9:高度
+                    //10:M
+                    //11:ジオイド高
+                    string[] cols = line.Split(',');
+                    temp.Add("Time", cols[1]);
+                    temp.Add("Latitude", Convert60to10(cols[2]));
+                    temp.Add("Longitude", Convert60to10(cols[4]));
+                    temp.Add("Quality", cols[6]);
+                    temp.Add("Satellites", cols[7]);
+                    temp.Add("GeoidHeight", cols[11]);
+
+                }
+            }
+
+            return temp;
+            
+        }
+
+        /// <summary>
+        /// GPS位置情報を60進数から10進数へ変換
+        /// </summary>
+        /// <param name="gpsPos"></param>
+        /// <returns></returns>
+        private String Convert60to10(String gpsPos)
+        {
+            //13599.12345 みたいになってる → 135と99.12345に分けて99.12345を60で割る
+            int searchPos = gpsPos.IndexOf('.');
+            if (searchPos > 0)
+            {
+                //小数点以下保持のためにdecimalでやってみた
+                decimal degree;
+                decimal.TryParse(gpsPos.Substring(0, searchPos - 2), out degree);
+                decimal minutes;
+                decimal.TryParse(gpsPos.Substring(searchPos - 2), out minutes);
+                decimal minutes10 = minutes / 60;
+                gpsPos = (degree + minutes10).ToString();
+            }
+            return gpsPos;
+        }
+        
+    }
+
+
+    #region イベントハンドラのデータ受け渡し
+    /// <summary>
+    /// イベントハンドラでデータを受け渡す為のクラス
+    /// </summary>
+    class GpsReadEventArgs : EventArgs
+    {
+        private readonly Dictionary<String, String> _args = null;
+
+        public GpsReadEventArgs(Dictionary<String, String> args)
+        {
+            this._args = args;
+        }
+
+        public Dictionary<String, String> args
+        {
+            get
+            {
+                return _args;
+            }
+        }
+
+        public String Time { get { return _args["Time"]; } }
+        public String Latitude { get { return _args["Latitude"]; } }
+        public String Longitude { get { return _args["Longitude"]; } }
+        public String Quality { get { return _args["Quality"]; } }
+        public String Satellites { get { return _args["Satellites"]; } }
+        public String GeoidHeight { get { return _args["GeoidHeight"]; } }
+
+    }
+    #endregion
+}
